d856bfeba7e7616d7b712d2f345e4b4c257c9292
iPAS <ptiwatthanont@gmail.com>
2018-07-26 13:50:28 +0700

    Try understanding the operation of ESP32's Slave-SPI API. Revise almost lines.

--- SlaveSPI.h
+++ SlaveSPI.h
@@ -8,56 +8,52 @@
 #define SPI_MODE 0
 #define SPI_DMA 0  // XXX: Still fail of use DMA whether 1 or 2 
 
+#define SPI_DEFAULT_MAX_BUFFER_SIZE 128
+
 // #define SPI_MALLOC_CAP (MALLOC_CAP_DMA | MALLOC_CAP_32BIT)
 #define SPI_MALLOC_CAP (MALLOC_CAP_DEFAULT | MALLOC_CAP_8BIT)
 
 class SlaveSPI {
 
+    friend void call_matcher_after_queueing(spi_slave_transaction_t * trans);
+    friend void call_matcher_after_transmission(spi_slave_transaction_t * trans);
+
   private:
     static SlaveSPI ** SlaveSPIVector;
-    static int         size;
+    static int         vector_size;
 
-    friend void setupIntr(spi_slave_transaction_t * trans);
-    friend void transIntr(spi_slave_transaction_t * trans);
+    String input_stream;     // Used to save incoming data
+    String output_stream;    // Used to buffer outgoing data
+    size_t max_buffer_size;  // Length of transaction buffer (maximum transmission size)
 
-    String buff;        // used to save incoming data
-    String transBuffer; // used to buffer outgoing data !not tested!
-    size_t t_size;      // length of transaction buffer, (should be set to maximum transition size)
+    spi_host_device_t spi_host;  // HSPI, VSPI
 
-    spi_host_device_t spi_host;  // HSPI, VSPI 
+    byte * tx_buffer;
+    byte * rx_buffer;
 
-    byte *txBuffer;
-    byte *rxBuffer;
-
-    spi_slave_transaction_t * driver;
-    void (*exter_intr)();  // interrupt at the end of transmission ,
-                           //  if u need to do something at the end of each transmission
+    spi_slave_transaction_t * transaction;
+    int (*callback_after_transmission)();  // Interrupt at the end of transmission,
+                                           //   if u need to do something at the end of each transmission
+    static int callbackDummy() { return 0; }
 
   public:
     SlaveSPI(spi_host_device_t spi_host=HSPI_HOST);  // HSPI, VSPI
 
-    void setup_intr(spi_slave_transaction_t * trans);  // called when the trans is set in the queue
-    void trans_intr(spi_slave_transaction_t * trans);  // called when the trans has finished
-
-    void begin(gpio_num_t so, gpio_num_t si, gpio_num_t sclk, gpio_num_t ss, size_t length = 128, void (*ext)() = NULL);
-    void trans_queue(String & transmission);  // used to queue data to transmit
-
-    inline char * operator[](int i) {
-        return &buff[i];
-    }
-
-    inline void flush() {
-        buff = "";
-    }
+    void callbackAfterQueueing(spi_slave_transaction_t * trans);      // Called when the trans is set in the queue
+    void callbackAfterTransmission(spi_slave_transaction_t * trans);  // Called when the trans has finished
 
     inline bool match(spi_slave_transaction_t * trans);
+    void initTransmissionQueue();
 
-    inline String * getBuff() {
-        return &buff;
-    }
+    void begin(gpio_num_t so, gpio_num_t si, gpio_num_t sclk, gpio_num_t ss,
+               size_t buffer_size = SPI_DEFAULT_MAX_BUFFER_SIZE, int (*callback)() = callbackDummy);
 
-    void setDriver();
-    char read();
+    void write(String & msg);  // Queue data then wait for transmission
+    byte read();
+
+    inline char *   operator[](int i) { return &input_stream[i]; }
+    inline String * getInputStream() { return &input_stream; }
+    inline void     flushInputStream() { input_stream = ""; }
 };
 
 #endif
--- test.ino
+++ test.ino
@@ -1,5 +1,6 @@
 /**
- * SlaveSPI Class from https://github.com/iPAS/esp32-slave-spi
+ * SlaveSPI Class
+ * - adopt from gist:shaielc/SlaveSPIClass.cpp at https://gist.github.com/shaielc/e0937d68978b03b2544474b641328145
  */
 #include "SlaveSPI.h"
 
@@ -20,15 +21,17 @@ SPISettings spi_setting(100000, MSBFIRST, SPI_MODE);
 
 SlaveSPI slave(HSPI_HOST);
 
-static String txt = "";
-static String cmd = "";
+static String slave_msg = "";
+static String master_msg = "";
 
-void slave_tx_finish() {
+int callback_after_slave_tx_finish() {
     // Serial.println("[slave_tx_finish] slave transmission has been finished!");
     // Serial.println(slave[0]);
+
+    return 0;
 }
 
-void print_hex(String str) {
+void printHex(String str) {
     for (int i = 0; i < str.length(); i++) {
         Serial.print(str[i], HEX);
         Serial.print(" ");
@@ -45,45 +48,45 @@ void setup() {
     digitalWrite(MS, HIGH);
     // slave.begin(SO, SI, SCLK, SS, 8, slave_tx_finish);  // seems to work with groups of 4 bytes
     // slave.begin(SO, SI, SCLK, SS, 4, slave_tx_finish);
-    slave.begin(SO, SI, SCLK, SS, 2, slave_tx_finish);
+    slave.begin(SO, SI, SCLK, SS, 2, callback_after_slave_tx_finish);
     // slave.begin(SO, SI, SCLK, SS, 1, slave_tx_finish);  // at least 2 word in an SPI frame
 }
 
 void loop() {
-    if (slave.getBuff()->length() && digitalRead(SS) == HIGH) {  // Slave SPI has got data in.
-        while (slave.getBuff()->length()) { 
-            txt += slave.read();
+    if (slave.getInputStream()->length() && digitalRead(SS) == HIGH) {  // Slave SPI has got data in.
+        while (slave.getInputStream()->length()) { 
+            slave_msg += slave.read();
         }
         Serial.print("slave input: ");
-        print_hex(txt);
+        printHex(slave_msg);
     }
 
     while (Serial.available()) {  // Serial has got data in 
-        cmd += (char)Serial.read();
+        master_msg += (char)Serial.read();
     }
 
-    while (txt.length() > 0) {  // Slave SPI output
-        slave.trans_queue(txt);
+    while (slave_msg.length() > 0) {  // Slave SPI output
+        slave.write(slave_msg);
         Serial.print("slave output: ");
-        print_hex(txt);
-        txt = "";
+        printHex(slave_msg);
+        slave_msg = "";
     }
 
-    while (cmd.length() > 0) {  // From serial to Master SPI
+    while (master_msg.length() > 0) {  // From serial to Master SPI
         Serial.print("serial input / master output: ");
-        Serial.println(cmd);
+        Serial.println(master_msg);
 
         Serial.print("master input (whether read or write mode): ");
         
         digitalWrite(MS, LOW);
         master.beginTransaction(spi_setting);
-        for (int i = 0; i < cmd.length(); i++) {
-            cmd[i] = master.transfer(cmd[i]);  // ERROR : gives the transmitted data <<1
+        for (int i = 0; i < master_msg.length(); i++) {
+            master_msg[i] = master.transfer(master_msg[i]);  // ERROR : gives the transmitted data <<1
         }
         master.endTransaction();
         digitalWrite(MS, HIGH);
         
-        print_hex(cmd);
-        cmd = "";
+        printHex(master_msg);
+        master_msg = "";
     }
 }
